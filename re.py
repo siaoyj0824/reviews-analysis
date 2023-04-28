909955c223d2ce2c00c867b4d0a1afcec0b4499c
siaoyj0824 <125232572+siaoyj0824@users.noreply.github.com>
2023-04-28 23:04:17 +0800

    Add files via upload

---
+++ re.py
@@ -0,0 +1,28 @@
+data = []
+count = 0
+with open('oringal.txt', 'r') as f:
+	for line in f:
+		data.append(line)  #增加
+		count = count + 1
+		if count % 1000 == 0:
+				print(len(data))
+print('檔案讀取完畢', '總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('平均留言長度', sum_len/len(data))
+
+#篩選留言<100
+new = []
+for d in data:
+	if len(d) < 100:
+			new.append(d)
+print('一共有', len(new), '留言長度小於100')
+print(new[0])
+
+good = []
+for d in data:
+	if 'good' in d:
+			good.append(d)
+print('一共有', len(good), '留言提到good')
